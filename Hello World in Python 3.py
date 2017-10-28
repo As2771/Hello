5ba9cbb6c8c6d4e534b75c8c0b2b53fb8b176917
LunarWatcher <joha.ingv@gmail.com>
2017-10-28 15:16:39 +0200

    Create Hello World in Python 3.py

---
+++ Hello World in Python 3.py
@@ -0,0 +1,8 @@
+# Printing "Hello World" in Python 3 is slightly different from Python 2:
+
+print("Hello, World!")
+'''
+The character (#) is used for comments. Both single and double quotes can be used in print statements.
+Three single quotes or double quotes following each other makes something that comes close to mutliline comments, but it's mostly used for documentation:
+https://stackoverflow.com/questions/7696924/way-to-create-multiline-comments-in-python
+'''
