3f221bd77c76467d8d02cd12a9d5f42f15b6c00f
Saurabh Daalia <saurabhdaalia98@gmail.com>
2017-10-28 18:56:03 +0530

    Maths Operation Using Python File added

---
+++ Maths_operations.py
@@ -0,0 +1,36 @@
+"""
+Author : Saurabh Daalia
+Country : India
+
+
+"""
+
+# Python Program for performing basic Mathematics Operations in Python 2.7"
+
+
+a=5.0
+b=3.0
+
+#Addition
+print "Sum of %d and %d is %d" %(a, b , a+b)
+#Output : Sum of 5 and 3 is 8
+
+#Subtraction
+print "Difference of %d and %d is %d" %(a,b,a-b)
+#Output : Difference of 5 and 3 is 2
+
+#Multiplication
+print "Product of %d and %d is %d" %(a,b,a*b)
+#Output : Product of 5 and 3 is 15
+
+#Divison
+print "%d divided by %d is %f" %(a,b,a/b)
+#Output : 5 divided by 3 is 1.666667
+
+#Remainder Operation
+print "Remainder of %d divided by %d is %d" %(a,b,a%b)
+#Oputput : Remainder of 5 divided by 3 is 2
+
+#Power
+print "%d raise to the power %d is %d" %(a,b,a**b)
+#Output : 5 raise to the power 3 is 125
