9c76033eedefe60a9d076b34efdd5b078b895eac
Denny <denny.aditya.p@gmail.com>
2017-10-28 22:34:38 +0700

    Added two ways on how to say Hello World using printf() and puts()

---
+++ HelloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	// There are 2 ways to print Hello World
+	printf("Hello World!");
+	printf(" Printf doesn't give you new line ");
+	// So, use \n for new lines
+	printf("\n");
+	puts("Hello World!");
+	puts("puts() gives you new line");
+	return 1;
+}
